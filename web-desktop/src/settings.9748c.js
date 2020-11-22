5c5aee0da31942f317736645f364eedaac04ce8a
yanjifa <yanjifa@joycastle.mobi>
2020-11-23 02:41:30 +0800

    update blog

---
+++ web-desktop/src/settings.9748c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"279e0",resources:"2f34c",main:"4ef6d"}};
